f2a50cdfb3dec313e554888b2c6990ee804dd79e
Niki <vogeln@bzz.ch>
2022-01-12 13:47:07 +0100

    Files

--- src/Managerin.java
+++ src/Managerin.java
@@ -1,2 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Vector;
+
 public class Managerin {
+    private Vector<int[]> datenArrays = new Vector<>();
+    public void getFileformFile(){
+        //BufferedReader reader = new BufferedReader(new File(""));
+    }
 }
