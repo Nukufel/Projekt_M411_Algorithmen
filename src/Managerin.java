15e690d8b05c08984411ff24c79b8b860a7ae42d
Niki <vogeln@bzz.ch>
2022-01-12 20:43:11 +0100

    Manegerin

--- src/Managerin.java
+++ src/Managerin.java
@@ -1,11 +1,32 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.lang.reflect.Array;
+import java.io.*;
 import java.util.Vector;
 
-public class Managerin {
+public class Managerin{
     private Vector<int[]> datenArrays = new Vector<>();
-    public void getFileformFile(){
-        //BufferedReader reader = new BufferedReader(new File(""));
+
+    private String[] dateiEndungen = new File(new File("").getAbsolutePath()+"/Testdaten").list();
+    private SorterInterface[] sortingClasses = {(SorterInterface) new BinaryTreeSort(), (SorterInterface)new HeapSort(),(SorterInterface) new InsertionSort(), (SorterInterface) new MergeSort(), (SorterInterface) new QuickSortPleft(), (SorterInterface) new ShakerSort()};
+
+    public Managerin(){
+
+    }
+
+    public void getFileformFile() throws IOException {
+        for (int i = 0; i<dateiEndungen.length; i++) {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("").getAbsolutePath() + "/Testdaten/" + dateiEndungen[i]));
+            datenArrays.add(readInToArray(reader));
+        }
+    }
+
+    public int[] readInToArray(BufferedReader reader) throws IOException {
+        int[] intArray = new int[(int)reader.lines().count()];
+        for (int j = 0; j<reader.lines().count(); j++){
+            intArray[j] = Integer.parseInt(reader.readLine());
+        }
+        return intArray;
+    }
+
+    public void sortingArrays() {
+        SorterInterface sorterInterface = (SorterInterface) new BinaryTreeSort();
     }
 }
