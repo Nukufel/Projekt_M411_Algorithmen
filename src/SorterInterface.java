7d143515e0b24559cc03e0e8d798870044e6291f
Niki <vogeln@bzz.ch>
2022-01-12 13:06:22 +0100

    Interface

---
+++ src/SorterInterface.java
@@ -0,0 +1,13 @@
+public interface SorterInterface {
+    public int[] getSortedArray();
+
+    public void sort(int[] zahlen);
+
+    public int getSpeicherbedarf();
+
+    public long getTime();
+
+    public int anzVergleiche();
+
+    public int anzahlSchreibzugriffe();
+}
