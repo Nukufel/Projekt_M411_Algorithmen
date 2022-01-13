bd7d3a8cb624bc6a255726cce5081d6a9525ff9a
Niki <vogeln@bzz.ch>
2022-01-13 19:22:43 +0100

    art to string für speicherung ist fertig

--- src/Managerin.java
+++ src/Managerin.java
@@ -3,10 +3,12 @@ import java.util.Vector;
 
 public class Managerin{
     private Vector<int[]> datenArrays = new Vector<>();
+    private Vector<Object[]> sortedArrays = new Vector<>();
 
     private String[] dateiEndungen = new File(new File("").getAbsolutePath()+"/Testdaten").list();
-    private SorterInterface[] sortingClasses = {(SorterInterface) new BinaryTreeSort(), (SorterInterface)new HeapSort(),(SorterInterface) new InsertionSort(), (SorterInterface) new MergeSort(), (SorterInterface) new QuickSortPleft(), (SorterInterface) new ShakerSort()};
-
+    private SorterInterface[] sortingClasses = {(SorterInterface) new QuickSortPrandom(), (SorterInterface) new BinaryTreeSort(), (SorterInterface)new HeapSort(),(SorterInterface) new InsertionSort(), (SorterInterface) new MergeSort(), (SorterInterface) new QuickSortPleft(), (SorterInterface) new ShakerSort()};
+    private String [] sorts = {"QuickSortPrandom", "BinaryTreeSort", "HeapSort", "InsertionSort", "MergeSort", "QuickSortPleft", "ShakerSort"};
+    private String[] arrayNames = {"InversTeilsortiert1000", "InversTeilsortiert10000","InversTeilsortiert100000","Random1000","Random10000","Random100000","Teilsortiert1000","Teilsortiert10000","Teilsortiert100000"};
     public Managerin(){
 
     }
@@ -27,6 +29,26 @@ public class Managerin{
     }
 
     public void sortingArrays() {
-        SorterInterface sorterInterface = (SorterInterface) new BinaryTreeSort();
+        for (int i = 0; i<sortingClasses.length; i++){
+            for (int[] dasArray: datenArrays) {
+                sortingClasses[i].sort(dasArray);
+                Object[] theArrayAndStuff = {sortingClasses[i].getSortedArray(), sortingClasses[i].anzahlSchreibzugriffe(), sortingClasses[i].anzVergleiche(), sortingClasses[i].getSpeicherbedarf(), sortingClasses[i].getTime()};
+                sortedArrays.add(theArrayAndStuff);
+            }
+        }
+    }
+
+    public void writeInToFile(){
+        String intoCSV = "";
+
+        for (int i = 0; i<7; i++){
+            intoCSV = sorts[i]+"\n";
+            intoCSV = ",Schreibzugriffe,Vergleiche,Speicherbedarf,Time\n";
+            for (int n = 0; n<9; n++) {
+                intoCSV = arrayNames[n]+","+sortedArrays.get((i*7)+n)[1]+","+sortedArrays.get((i*7)+n)[2]+","+sortedArrays.get((i*7)+n)[3]+","+sortedArrays.get((i*7)+n)[4]+"\n";
+            }
+        }
+
+
     }
 }
